95cab82b20d8f0f7ca491e73c83a9449b15df94b
haydenso <sohayden929@gmail.com>
2025-12-19 23:52:01 +0800

    added scmp redirect

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/scmp/:path*',
+        destination: 'https://haydenso.github.io/scmp/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig
